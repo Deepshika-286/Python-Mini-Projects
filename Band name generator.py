efe0615909dc7d19ea412e6e0ffaeeeb80be068f
deepshika-286 <deepshikareddy286@gmail.com>
2025-04-17 16:14:45 +0530

    band name generator

---
+++ Band name generator.py
@@ -0,0 +1,6 @@
+# printing a band name based on city and pet names:
+print("Get your band names!")
+city=input("Type yr city name:")
+pet=input("What's the name of your pet:")
+print("Your band name is",city,pet+"'s!")
+#example: nyc rabbit's!ū
